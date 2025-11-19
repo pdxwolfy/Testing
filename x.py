e4b8c2ccd7f6893cc1db29b0f4f48ad1934fe4d5
pdxwolfy <oregon.wolfy@gmail.com>
2025-11-18 18:17:59 -0800

    test

--- x.py
+++ x.py
@@ -1,11 +1,1 @@
-def multiply(left, right):
-    return left * right + 3
-
-def get_number(prompt):
-    entry = input(prompt)
-    return float(entry)
-
-first_number = get_number('Enter the first number: ')
-second_number = get_number('Enter the second number: ')
-product = multiply(first_number, second_number)
-print(f'{first_number} * {second_number} = {product}')
+ 
